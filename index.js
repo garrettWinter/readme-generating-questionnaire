bbe9a9a19e6975244b0321b4d72fe4e50ad8b348
Garrett Winter <Garrett_Winter@hotmail.com>
2022-12-21 00:01:01 -0600

    Setup Inquirer to collect basic user data

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-// TODO: Include packages needed for this application
+/* Global Variables */
+let inquirerData;
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {
+    fs.writeFile('README.md',
+    `DETAILS TO BE WRITTEN TO FILE`
+    , (err) =>
+    err ? console.error(err) : console.log('README has been created!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    detailsGathering();
+    // writeToFile();
+}
+
+function detailsGathering(){   
+/*
+    Collect User Data
+    GitHub username, email address
+
+    Collect Readme Data
+    Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+*/
+inquirer
+.prompt([
+  {
+    type: 'input',
+    message: 'What is your GitHub user name?',
+    name: 'gitHub',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+])
+.then((response) => {
+    inquirerData = {...response};
+    console.log(inquirerData);
+});
+
+}
 
 // Function call to initialize app
 init();
