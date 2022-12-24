cff029e6a7f379238b31aafc98f9bc0971653cb7
Garrett Winter <Garrett_Winter@hotmail.com>
2022-12-24 00:57:28 -0600

    Started inquirer prompts and have worked out repeat logic.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /* Global Variables */
 const inquirer = require('inquirer');
 const fs = require('fs');
-let collectedData;
+let collectedData1 = [];
+let collectedData2 = [];
+let collectedData3 = [];
+let descMotivationArray = [];
+let descLearnArray = [];
 
 
 
@@ -15,16 +19,16 @@ function writeToFile() {
 
 // TODO: Create a function to initialize app
 function init() {
-    detailsGathering();
+    userDetailsGatheringPart1();
 }
 
-function detailsGathering(){   
+function userDetailsGatheringPart1(){   
 /*
     Collect User Data
     GitHub username, email address
 
     Collect Readme Data
-    Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    Table of Contents
 */
 inquirer
 .prompt([
@@ -38,14 +42,109 @@ inquirer
     message: 'What is your email address?',
     name: 'email',
   },
+  {
+    type: 'confirm',
+    message: 'Include table of Contents?',
+    name: 'tableContents',
+  },
+  {
+    type: 'input',
+    message: 'What is the Project Name?',
+    name: 'projectName',
+  },
+  {
+    type: 'input',
+    message: 'What problem does it solve?',
+    name: 'descSolve',
+  },
 ])
-.then((response) => {
-    collectedData = {...response};
-    console.log(collectedData);
-    writeToFile();
+  .then((response) => {
+    collectedData1 = {...response};
+    userDetailsGatheringPart2();
 });
-
 }
+function userDetailsGatheringPart2(){   
+    /*
+        Collect Readme Data
+        Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    */
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What was your motivation?',
+            name: 'descMotivation',
+          },
+        {
+            type: "confirm",
+            name: "repeat",
+            message: "Would you like to add another motivation bullet?",
+          },
+    ])
+      .then((response) => {
+        collectedData2 = response;
+        if (response.repeat === true){
+            descMotivationArray.push(collectedData2.descMotivation);
+            console.log(collectedData1);
+            console.log(collectedData2);
+            console.log(descMotivationArray);
+            userDetailsGatheringPart2();
+            return;
+        }
+        userDetailsGatheringPart3()
+    });
+    }
+    function userDetailsGatheringPart3(){   
+        /*
+            Collect Readme Data
+            Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+        */
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What did you learn?',
+                name: 'descLearn',
+              },
+            {
+                type: "confirm",
+                name: "repeat",
+                message: "Would you like to add another learned bullet?",
+              },
+        ])
+          .then((response) => {
+            collectedData3 = response;
+            if (response.repeat === true){
+                descLearnArray.push(collectedData3.descLearn);
+                console.log(collectedData1);
+                console.log(collectedData2);
+                console.log(collectedData3);
+                console.log(descLearnArray);
+                userDetailsGatheringPart3();
+                return;
+            }
+            // writeToFile();
+        });
+        }
+
+
+
+
+
+  
+
+
+
+
+  
+// ])
+// .then((response) => {
+//     collectedData = {...response};
+//     console.log(collectedData);
+//     writeToFile();
+// });
+
+// }
 
 // Function call to initialize app
 init();
